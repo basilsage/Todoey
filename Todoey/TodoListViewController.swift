50719fc9f97e6328997f2accf6461b549d1503a4
DJ Satoda <dj@DJs-MacBook-Pro.local>
2019-05-04 17:47:40 -0700

    Added 'Add new item' functionality

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    //MARK - Tableview Datasource methods
+    //MARK: - Tableview Datasource methods
     // These tell tableview what cells should display and how many rows
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,7 +33,7 @@ class TodoListViewController: UITableViewController {
     }
     
     
-    // MARK - Tableview Delegate Methods
+    // MARK: - Tableview Delegate Methods
     // These get fired whenever we click on any cell in tableview
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -44,9 +44,37 @@ class TodoListViewController: UITableViewController {
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
-//        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         tableView.deselectRow(at: indexPath, animated: true)
         }
+
+    // MARK: - Add New Item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what will happen when user clicks add item in UI alert
+            self.itemArray.append(textField.text!)
+            
+            // updates tableview for new rows
+            self.tableView.reloadData()
+        }
+        
+        // Adds text field to alert view controller
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item" // sample text in field
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    
+    }
+    
     
 }
 
